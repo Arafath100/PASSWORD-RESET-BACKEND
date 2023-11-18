93d6ba3742930f85ce186116b37cfcca5d3fbcf1
Arafath <arafath2092@gmail.com>
2023-11-18 15:48:49 +0530

    Done

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../Controllers/userController');
 const verifyToken = require('../Middleware/verifyToken');
-const avatarController = require('../Controllers/avatarController');
 
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 router.post('/forgot-password', userController.forgotPassword);
 router.put('/reset-password', userController.resetPassword);
-router.get('/avatars', avatarController.getAllAvatars);
 router.get('/verify-token/:token', userController.verifyToken);
 
 // Protected route - example
